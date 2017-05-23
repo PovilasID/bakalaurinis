14686fb1a0e1269c48cf0f3da2d2a5b50b0faf3f
Povilas Sid <povilasid@gmail.com>
2017-05-23 23:52:58 +0300

    Correct table implementation with passed buttons

--- src/app/components/user/condition_overview.jsx
+++ src/app/components/user/condition_overview.jsx
@@ -9,13 +9,24 @@ import Loading from '../helpers/loading';
 import ConditionChart from './condition_chart';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import {DataTable} from 'react-data-components';
+require('react-bootstrap-table/css/react-bootstrap-table.css')
 
-        const renderRecomendations =
-            (val, row) =>
-              <Link to="pef/booo">
-                <button type="button" class="btn btn-info">Recomendations</button>
-              </Link>;
+class ActionFormatter extends React.Component {
+  render() {
+    return (
+      <button className='btn btn-info'>Action</button>
+    );
+  }
+}
 
+function actionFormatter(cell, row) {
+    console.log("ACTION DATA", cell);
+  return (
+        <Link to={'pef/'+cell}>
+            <button type="button" className="btn btn-info">Recomendations</button>
+        </Link>
+    );
+}
 class ConditionOverview extends Component {
 
     constructor(props) {
@@ -25,39 +36,56 @@ class ConditionOverview extends Component {
         this.state = {
             message: '',
             data: [
-            { name: 'name ', city: 'city value', address: 'address value', phone: 'phone value' }
-            ],
-            columns : [
-              { title: 'Name', prop: 'name'  },
-              { title: 'City', prop: 'city' },
-              { title: 'Address', prop: 'address' },
-              { title: 'Recomendations', render: renderRecomendations, className: 'text-center' },
+            { pef: 300, date: 'city value', id: 'dfgfgd', },
+            { pef: 500, date: 'city value', id: '0000', }
+
             ],
             products:[],
+            pefRaw: {},
         };
     }
 
 
+    componentDidMount(){
+        firebaseDb.ref("pef").child(this.props.patient).on('value', snap =>{
+            console.log("PEF in overview", snap.val());
+            this.setState({pefRaw: snap.val()
+            });
+        });
+    }
+    renderRecomendations(){
+    return (
+        <Link to="pef/booo">
+            <button type="button" className="btn btn-info">Recomendations</button>
+            </Link>
+            );
+    }
 
 
     render() {
         if (!this.props.currentUser ) {
             return <Loading />;
         }
-
+        var products = [{
+              id: 1,
+              name: "Product1",
+              price: 120
+          }, {
+              id: 2,
+              name: "Product2",
+              price: 80
+          }];
         return (
 
             <div className="col-md-12">
                 
-                <ConditionChart patient={this.props.patient}/>
+                <ConditionChart patient={this.props.patient} pefRaw={this.state.pefRaw}/>
 
-                <DataTable
-                  keys="name"
-                  columns={this.state.columns}
-                  initialData={this.state.data}
-                  initialPageLength={5}
-                  initialSortBy={{ prop: 'city', order: 'descending' }}
-                />
+                  <BootstrapTable data={this.state.data} striped hover pagination>
+                      <TableHeaderColumn dataSort={ true } dataField='pef'>Product ID</TableHeaderColumn>
+                      <TableHeaderColumn dataSort={ true } dataField='date'>Product Name</TableHeaderColumn>
+                      <TableHeaderColumn isKey dataField='id'  dataFormat={ actionFormatter }>Product Price</TableHeaderColumn>
+                  </BootstrapTable>
             </div>
         );
     }
