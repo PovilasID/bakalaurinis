c2f11bffaf99108d0f6cb5ca417834f01cae0346
PovilasID <povilasid@gmail.com>
2017-05-25 00:42:48 +0300

    Style submit

--- src/app/components/user/condition_summary.jsx
+++ src/app/components/user/condition_summary.jsx
@@ -15,6 +15,7 @@ class ConditionSummary extends Component {
         fullSummary: '',
         summaryText: '',
         summaryType: '',
+        norms:'',
 
     };
   }
@@ -40,15 +41,44 @@ class ConditionSummary extends Component {
       });
       chartDataRaw = chartDataRaw.sort(Comparator)
       lastPEF = chartDataRaw[Object.keys(chartDataRaw)[0]];
+      if(this.state.norms != ''){
+        if(lastPEF[1] <= this.state.norms["min"]){
+          console.log("BELOW MIN");
+          this.setState({
+            summaryText: <div><h2><span className="label label-danger">WARNING! </span></h2> <p> Your last PEF meassument was <span className="label label-danger">{lastPEF[1]} </span></p>
+            <p>Its CRITICLY low plase consider taking imediate action. Visit your doctors or refer to your action plan.</p></div>,
+          });
+
+        }else if (lastPEF[1] > this.state.norms["min"] && lastPEF[1] < this.state.norms["mid"]) {
+
+          this.setState({
+
+            summaryText: <div><h2><span className="label label-warning">CAUSION!</span></h2> <p>Your last PEF meassument was <span className="label label-warning"> {lastPEF[1]} </span></p>
+            <p>Its below avearge and the day of meassument you may be more suceptable to alergens, physical or emotional stress.
+            Please be careful.</p></div>,
+          });
+        }else if (lastPEF[1] >= this.state.norms["mid"] ) {
+
+          this.setState({
+            summaryText: <div><h2><span className="label label-success">Congradulations!</span></h2> <p> Your last PEF meassument was <span className="label label-success">{lastPEF[1]}</span></p>
+            <p>Its above avearge and the day of meassument you should be more resiliant to alergens, physical or emotional stress.
+            Enjoy your day.</p></div>,
+          });
+        }
+        //console.log("CALL NT NULL", snap.val()["pefNorms"]["min"]);
+
+      }else{
       this.setState({
         summaryText: "Your last PEF meassument was: "+lastPEF[1],
       });
+    }
       console.log("last pEF", lastPEF[1]);
     });
 
     firebaseDb.ref("settings").child(this.props.currentUser.uid).on('value', snap =>{
       if(snap.val() != null){
         var norms = snap.val()["pefNorms"];
+        this.setState({norms: norms});
         if(lastPEF[1] <= norms["min"]){
           console.log("BELOW MIN");
           this.setState({
