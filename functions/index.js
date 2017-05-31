12e0e1a6c8470afcfd6ed57b9ab31c3a6812664d
Povilas Sid <povilasid@gmail.com>
2017-05-31 19:21:49 +0300

    Deploy block

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,12 @@
 var functions = require('firebase-functions');
 
-exports.setLatest = functions.database
-	.ref('/pef/{userId}')
-	.orderByChild('timestamp')
-	.on(event => {
-		const pef = event.data.val()
-		console.log("ALL THE PEFS I GOT", pef);
-		console.log("LATEST PEF", Object.keys(pef)[0]);
+// exports.setLatest = functions.database
+// 	.ref('/pef/{userId}')
+// 	.orderByChild('timestamp')
+// 	.on(event => {
+// 		const pef = event.data.val()
+// 		console.log("ALL THE PEFS I GOT", pef);
+// 		console.log("LATEST PEF", Object.keys(pef)[0]);
 		
-		//ref.child('videos/videoId1').
-	})
+// 		//ref.child('videos/videoId1').
+// 	})
