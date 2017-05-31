4ff80035b9aeedc63bc379603ac23d76976653c2
PovilasID <povilasid@gmail.com>
2017-05-31 17:59:00 +0300

    Latest PEF getting cloud function test

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 var functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.setLatest = functions.database
+	.ref('/pef/{userId}')
+	.orderByChild('timestamp')
+	.on(event => {
+		const pef = event.data.val()
+		console.log("ALL THE PEFS I GOT", pef);
+		console.log("LATEST PEF", Object.keys(pef)[0]);
+		
+		//ref.child('videos/videoId1').
+	})
