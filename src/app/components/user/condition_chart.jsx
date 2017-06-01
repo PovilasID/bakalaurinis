019928e1a1b61361635d604998508b2b78569a23
PovilasID <povilasid@gmail.com>
2017-06-01 19:27:52 +0300

    Date filtering for a chart

--- src/app/components/user/condition_chart.jsx
+++ src/app/components/user/condition_chart.jsx
@@ -4,6 +4,10 @@ import { bindActionCreators } from 'redux';
 import { submitCondition } from '../../actions/firebase_actions';
 import {firebase,firebaseDb} from '../../utils/firebase';
 import { Chart } from 'react-google-charts';
+import moment from 'moment';
+import Datetime from 'react-datetime';
+
+import 'react-datetime/css/react-datetime.css';
 
 class ConditionChart extends Component {
 
@@ -11,6 +15,8 @@ class ConditionChart extends Component {
       super(props);
       this.state = {
         message: '',
+        startDate : moment().subtract(1, 'month').valueOf(),
+        endDate: moment().valueOf(),
         options: {
         title: 'PEF change overtiem',
         hAxis: { title: 'Time'},
@@ -32,17 +38,18 @@ class ConditionChart extends Component {
     };
   }
 
-
-  componentDidMount(){
-
-     function Comparator(a, b) {
+  populateChart(){
+    function Comparator(a, b) {
        if (a[0] < b[0]) return -1;
        if (a[0] > b[0]) return 1;
        return 0;
      }
     console.log("USER ID IN CHARTS", this.props.patient);
     console.log("PEF PROPS in CHARTS", this.props.pefRaw);
-    firebaseDb.ref("pef").child(this.props.patient).on('value', snap =>{
+
+    var startDate = moment().subtract(2, 'days').valueOf();
+    var endDate = moment().valueOf();
+    firebaseDb.ref("pef").child(this.props.patient).orderByChild("timestamp").startAt(this.state.startDate).endAt(this.state.endDate).on('value', snap =>{
       var data = snap.val();
       if(data){
         console.log("PEF DATA IN CHART", data);
@@ -63,18 +70,50 @@ class ConditionChart extends Component {
     });
   }
 
+  handleStartDateChenge(date){
+    this.setState({startDate:date.valueOf()});
+    this.populateChart();
+  }
+  handleEndDateChenge(date){
+    this.setState({endDate:date.valueOf()});
+    this.populateChart();
+  }
+  componentWillMount(){
+    this.populateChart();
+
+  }
+
 
     render() {
       return (
+        <div>
 
-      <Chart
-        chartType="LineChart"
-        data={this.state.data}
-        options={this.state.options}
-        width="100%"
-        height="400px"
-        legend_toggle
-      />
+          <form className="form-inline">
+              <Datetime className="form-group" id="exampleInputName2"
+                value={this.state.startDate} 
+                dateFormat="YYYY-MM-DD" 
+                timeFormat={false} 
+                isValidDate={(current) => current.isBefore(Datetime.moment())}
+                onChange={this.handleStartDateChenge.bind(this)} 
+              />
+              <Datetime 
+                className="form-group" id="exampleInputEmail2"
+                value={this.state.endDate} 
+                dateFormat="YYYY-MM-DD" 
+                timeFormat={false} 
+                isValidDate={(current) => current.isBefore(Datetime.moment())}
+                onChange={this.handleEndDateChenge.bind(this) } 
+              />
+          </form>
+          <Chart
+            chartType="LineChart"
+            data={this.state.data}
+            options={this.state.options}
+            width="100%"
+            height="400px"
+            legend_toggle
+          />
+      </div>
     );
   }
 
